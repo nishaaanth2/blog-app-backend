b350ed461f1e0f5531265a133f4b3e4df99e20e1
nyow <itismenishaanth@gmail.com>
2022-06-13 19:46:50 +0530

    mongoose connected

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 console.log("hello")
 const app = require("express")()
+var mongoose = require('mongoose');
+const dbConfig = require('./src/config/configDatabase');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
 //const cors = require("cors")
 const PORT = 3000;
 const express = require("express");
 const router = require('./src/routes/index')
-//app.use(cors());
+const env = process.env.NODE_ENV || 'local';
 
 app.use(express.json())
 app.use('/api/v1',router)
